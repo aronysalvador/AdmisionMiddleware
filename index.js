e124e518e9b85f96095372857168e8a59c55c9b7
Díaz Gareca <sadiazg@achs.cl>
2020-07-08 17:16:20 -0400

    Access-Control-Allow-Origin

--- index.js
+++ index.js
@@ -14,5 +14,11 @@ app.use(express.json())
 app.use(morgan('dev')) 
 app.use('/api/patient', patient) 
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); 
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  })
+
 const port = 80;
 app.listen(port, () => { console.log(`Listen on port ${port}`) })
