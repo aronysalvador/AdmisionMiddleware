705f52e72974501dfca67abeed72bdff81e344d3
Tony <tony.krikorian@outlook.com>
2020-07-21 11:00:01 -0400

    Add get isapres request

---
+++ Request/isapres.js
@@ -0,0 +1,11 @@
+const getIsapres = () => {
+  return {
+    hostname: "sfc-qa-middleware.eastus.cloudapp.azure.com:8080",
+    path: `/api/dominios/ObtenerListadoIsapres`,
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "Ocp-Apim-Subscription-Key": "2b4beff42a5b433cad5bb1bc67b81fc3",
+    },
+  };
+};
+module.exports = getIsapres;
