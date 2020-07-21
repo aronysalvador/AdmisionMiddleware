69e13e3d366135f325772955b853607302a34474
Tony <tony.krikorian@outlook.com>
2020-07-21 17:10:59 -0400

    Controller

---
+++ routes/cargos.js
@@ -0,0 +1,15 @@
+const Router = require("express-promise-router");
+const apiResponse = require("../Utils/ApiUtil/apiResponseReducer");
+const cargosRequest = require("../Request/cargos");
+const http = require("../Utils/ApiUtil/http");
+const data = require("../Utils/cargos.json");
+
+const route = new Router();
+
+route.get("/", async (req, res) => {
+  try {
+    res.send(data);
+  } catch (error) {
+    const result = apiResponse([], 500, "Ha ocurrido un error");
+  }
+});
