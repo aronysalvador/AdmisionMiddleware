b9ba2a29453d27b6daf38aeae27e27d690ddb1b2
Tony <tony.krikorian@outlook.com>
2020-07-21 17:06:45 -0400

    Cargos request

---
+++ Request/cargos.js
@@ -0,0 +1,12 @@
+const getCargos = () => {
+  return {
+    hostname: "",
+    path: "",
+    headers: {
+      headers: {
+        "Ocp-Apim-Subscription-Key": "2b4beff42a5b433cad5bb1bc67b81fc3",
+      },
+    },
+  };
+};
+module.exports = getCargos;
