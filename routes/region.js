d1b162eccd4a9198c875510520fba66672c891a6
Tony <tony.krikorian@outlook.com>
2020-07-28 10:03:38 -0400

    Add service regiones

---
+++ routes/region.js
@@ -0,0 +1,16 @@
+const Router = require("express-promise-router");
+const regionesRequest = require("../Request/regiones");
+const http = require("../Utils/ApiUtil/http");
+const regiones = require("../Utils/regiones.json");
+
+const route = new Router();
+
+route.get("/", async (req, res) => {
+  try {
+    const response = apiResponse(regiones, 200, "Operacion exitosa");
+    return res.send(response);
+  } catch (error) {
+    const response = apiResponse([], res.status, "Error");
+    return res.send(response);
+  }
+});
