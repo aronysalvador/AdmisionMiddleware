69dc7090b90065162dde696dc351df8823c2ae0f
Tony <tony.krikorian@outlook.com>
2020-07-28 10:10:47 -0400

    Add request comuna

---
+++ Request/comunas.js
@@ -0,0 +1,12 @@
+const getConfigComunas = () => {
+  return {
+    hostname: "http://sfc-desa-middleware.eastus.cloudapp.azure.com:8080",
+    path: "/api/dominios/ObtenerListadoRegiones",
+    headers: {
+      headers: {
+        "Ocp-Apim-Subscription-Key": "2b4beff42a5b433cad5bb1bc67b81fc3",
+      },
+    },
+  };
+};
+module.exports = getConfigComunas;
