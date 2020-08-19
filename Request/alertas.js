3b3f454547f1223d9c6f16c673752c0f1ccd561c
Díaz Gareca <sadiazg@achs.cl>
2020-08-18 20:07:02 -0400

    integración api alertas

--- Request/alertas.js
+++ Request/alertas.js
@@ -1,7 +1,8 @@
-const getAlertas = () => {
+
+const getNivel1 = () => {
   return {
-    hostname: "",
-    path: "",
+    hostname: "https://ams-qa-midleware.azure-api.net",
+    path: "/AdmisionDigital/api/Dominios/ObtenerAlertadeCalificacionNivel1",
     headers: {
       headers: {
         "Ocp-Apim-Subscription-Key": "2b4beff42a5b433cad5bb1bc67b81fc3",
@@ -9,4 +10,29 @@ const getAlertas = () => {
     },
   };
 };
-module.exports = getAlertas;
+
+const getNivel2 = () => {
+  return {
+    hostname: "https://ams-qa-midleware.azure-api.net",
+    path: "/AdmisionDigital/api/Dominios/ObtenerAlertadeCalificacionNivel2",
+    headers: {
+      headers: {
+        "Ocp-Apim-Subscription-Key": "2b4beff42a5b433cad5bb1bc67b81fc3",
+      },
+    },
+  };
+};
+
+const getNivel3 = () => {
+  return {
+    hostname: "https://ams-qa-midleware.azure-api.net",
+    path: "/AdmisionDigital/api/Dominios/ObtenerAlertadeCalificacionNivel3",
+    headers: {
+      headers: {
+        "Ocp-Apim-Subscription-Key": "2b4beff42a5b433cad5bb1bc67b81fc3",
+      },
+    },
+  };
+};
+
+module.exports = {getNivel1,getNivel2,getNivel3};
--- routes/alertas.js
+++ routes/alertas.js
@@ -1,17 +1,42 @@
 const Router = require("express-promise-router");
-const apiResponse = require("../Utils/ApiUtil/apiResponseReducer");
-const alertasRequest = require("../Request/alertas");
-const http = require("../Utils/ApiUtil/http");
-const data = require("../Utils/alertas.json");
+const apiResponse = require("../Utils/ApiUtil/apiResponseObjectReducer");
+const  {getNivel1,getNivel2,getNivel3} = require('../Request/alertas')
+const get = require('../Utils/ApiUtil/http')
 
 const route = new Router();
 
 route.get("/", async (req, res) => {
-  try {
-    res.send(data);
-  } catch (error) {
-    res.send(apiResponse([], 500, "Ha ocurrido un error"));
-  }
+    try {
+      
+      //obtenemos los niveles
+      const responseN1 = await get(getNivel1())
+      const responseN2 = await get(getNivel2())
+      const responseN3 = await get(getNivel3())
+      
+      //creamos la estructura
+      const option3 = getStructure(responseN3,[], 0)
+      const option2 = getStructure(responseN2,option3, 1)
+      const structure = getStructure(responseN1,option2, 1)
+      
+      //resultado
+      res.send(apiResponse(structure,200,"Operación Exitosa"))
+
+    } catch (error) {
+      res.send(apiResponse([], 500, error))
+   }
 });
+
+const getStructure = (data,childData,key) => {
+  const estructura = data.map(reg => {
+    let opciones = []
+    
+    if(reg.key === key)
+      opciones = childData
+
+    return {"id": reg.key, "glosa": reg.value, "opciones": opciones}
+  })
+
+  return estructura
+}
 
 module.exports = route;
