53aba84e42024d78d6e8b03862d0b8d38fd402db
Díaz Gareca <sadiazg@achs.cl>
2020-08-25 08:57:31 -0400

    ObtenerListadoTiposRemuneracion

--- Request/tipoRemuneracion.js
+++ Request/tipoRemuneracion.js
@@ -1,7 +1,7 @@
 const getConfigTipoRemuneracion = () => {
   return {
     hostname: "https://ams-qa-midleware.azure-api.net",
-    path: "/AdmisionDigital/TipoIngreso",
+    path: "/AdmisionDigital/api/Dominios/ObtenerListadoTiposRemuneracion",
     headers: {
       headers: {
         "Ocp-Apim-Subscription-Key": "2b4beff42a5b433cad5bb1bc67b81fc3",
